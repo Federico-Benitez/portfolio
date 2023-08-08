e060f7bcb224386db3229c09ee032c73fc6a79ad
Federico Benitez <fedebenitez1696@gmail.com>
2023-08-08 19:42:34 -0300

    feat: files for pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default function About() {
   return (
     <main>
-      <h1>Bienvenido</h1>
+      <h1>About</h1>
     </main>
   )
 }
